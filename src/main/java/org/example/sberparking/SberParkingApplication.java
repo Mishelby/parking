9d31e037c43da7b77dcc3dd695b6b91f2ab5190d
Misha <lorestatys@yandex.ru>
2025-04-19 12:14:38 +0300

    init commit

---
+++ src/main/java/org/example/sberparking/SberParkingApplication.java
@@ -0,0 +1,13 @@
+package org.example.sberparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberParkingApplication.class, args);
+    }
+
+}
